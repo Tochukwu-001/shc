b442608525361f6150654d507e04f0bf5d4173e4
Tochukwu Ozo-Osiedo <tochukwuosiedo001@gmail.com>
2026-01-09 13:01:05 +0100

    updates

--- components/Styles.jsx
+++ components/Styles.jsx
@@ -1,5 +1,6 @@
 export const Theme = {
-    darkTheme: "#C5A047",
+    darkYellow: "#C5A047",
     lightCyan: "#00897B",
-    darkYellow: "#333333"
+    darkTheme: "#333333",
+    baseGreen: "#166534"
 }
